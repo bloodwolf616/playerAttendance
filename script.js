64a13745c3e4a8fd48925fba8c86ea31da1f8691
bloodwolf616 <54482130+bloodwolf616@users.noreply.github.com>
2020-04-11 22:23:33 -0700

    Making the code more simple and easy to read.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+//starting variables 
+const playerList = document.getElementById('playerList');
+const output = document.createElement('div');
+output.className = 'player-container'
+
+
+
+
+
 /*using javascript class syntax to create and manage numerous objects at the same time.
  the Class' first letter has to be upper case to differenciate from functions and variables*/
 class Player {
@@ -5,7 +14,7 @@ class Player {
     constructor(name,age,online=false) {
         this.name = name;
         this.age = age;
-        //this.online = online;
+        
     }
 //creates a method we can use for these objects that do special tasks
 
@@ -22,14 +31,9 @@ class Player {
 }
 
 /*---------------------------------------------------------------
-end
+end of Class
 ------------------------------------------------------------------*/
 
-//selecting the span button and the paragraph. then using jquery to make the paragraph disapppear when the user clicks on span element
-const alertBox = $('.alert-box');
-const $closeAlert = $('.close-alert').on('click', function() {
-  alertBox.slideUp(2000);
-})
 
 
 //creating a new object from scratch using javascript class syntax. You can create objects very fast this way.
@@ -42,10 +46,7 @@ const player5 = new Player('Constantine', '21');
 const player6 = new Player('Elizabeth', '24');
 const player7 = new Player('Marco', '28');
 const player8 = new Player('Bill', '45');
-// player1._online = true;
-// player2._online = true;
-// player3._online = true;
-// player4._online = true;
+
 
 //storing my objects in a array
 const attendance = [player1, error, player2, player3, player5, player7, player6, player4, player8];
@@ -77,7 +78,7 @@ if  (hour > 10 && hour < 20) {
  }
 
 //the online and offline times for player6-8
- if  (hour > 20 && hour < 24) {
+ if  (hour > 18 && hour < 24) {
     player6._online = true;
     player7._online = true;
     player8._online = true;
@@ -119,17 +120,12 @@ if  (hour > 10 && hour < 20) {
               if(error) {error._online = false;}}
 
 
-/*---------------------------------------------------------------
-end
-------------------------------------------------------------------*/
+
 
 /*-------------------------------------------------------------------
-Using a loop to create my player list, put the vlaue of the object in the elements, then attach it to a div element with the class 'player-container'. then attach the fiv container to the main element
+Using a loop to create my player list by going through my attendance array. put the value of the objects into elements, attach them to an unordered list. 
 ----------------------------------------------------------------------*/
 
-const playerList = document.getElementById('playerList');
-const output = document.createElement('div');
-output.className = 'player-container'
 
 
 // Can style elements in the loop
@@ -138,13 +134,16 @@ for (let i = 0; i < attendance.length; i++) {
   var playerProfile = document.createElement('UL');
   playerProfile.className = 'flex-container';
 
+  //creating 5 nodes that hold the players name, age, and onlnie status. Each node will be given a class so that they can be styled
     var node1 = document.createElement('strong');
     node1.innerHTML = '<strong>' + 'Name:' + '</strong>';
     node1.className = 'name';
 
+
     var node2 = document.createElement('LI');
     node2.innerHTML = '<li>' + attendance[i].name + '</li>';
     node2.className = 'profile-name';
+
 
     var node3 = document.createElement('strong');
     node3.innerHTML = '<strong>' + 'Age:' + '</strong>';
@@ -155,9 +154,12 @@ for (let i = 0; i < attendance.length; i++) {
     node4.innerHTML = '<li>' + attendance[i].age + '</li>';
     node4.className = 'profile-age';
 
+
     var node5 = document.createElement('LI');
     node5.innerHTML = '<li>' + attendance[i].online + '</li>';
 
+
+    //If the value of the online is true it will have the textContent of 'online' and be styled one way. If not it have the texContent of 'offline' will be styled another way
     if (attendance[i].online  === true) {
         node5.textContent = 'Online';
         node5.className = 'style-online';
@@ -167,13 +169,25 @@ for (let i = 0; i < attendance.length; i++) {
         node5.className = 'style-offline';
     }
 
+    //attaching my elements to an unordered list
     playerProfile.appendChild(node1);
     playerProfile.appendChild(node2);
     playerProfile.appendChild(node3);
     playerProfile.appendChild(node4);
     playerProfile.appendChild(node5);
-
+   //attaching my unordered list to a variable named output which is a div element
     output.appendChild(playerProfile);
 }
 
+//attaching my div element to the main element in index.html
 playerList.appendChild(output);
+
+
+
+//selecting the span button and the paragraph. then using jquery to make the paragraph disapppear when the user clicks on span element
+const alertBox = $('.alert-box');
+const $closeAlert = $('.close-alert').on('click', function() {
+  alertBox.slideUp(2000);
+})
+
+
